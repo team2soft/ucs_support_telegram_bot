329d6ffc5d4614b16db73f28ebb27bb45ff33dc1
Konstantin <konstantin.sergey@mustpay.tech>
2024-03-27 16:29:39 +0200

    Added menu

--- src/main/java/com/ukrcarservice/UcsSupportBot/service/TelegramBot.java
+++ src/main/java/com/ukrcarservice/UcsSupportBot/service/TelegramBot.java
@@ -1,17 +1,37 @@
 package com.ukrcarservice.UcsSupportBot.service;
 
 import com.ukrcarservice.UcsSupportBot.config.BotConfig;
-import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @Component
-@RequiredArgsConstructor
 public class TelegramBot extends TelegramLongPollingBot {
-    final BotConfig config;
+    private final BotConfig config;
+
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+        List<BotCommand> commands = new ArrayList<>();
+        commands.add(new BotCommand("/start", "welcome message"));
+        commands.add(new BotCommand("/help", "how to use this bot"));
+        commands.add(new BotCommand("/settings", "set your preferences"));
+
+        try {
+            this.execute(new SetMyCommands(commands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bots command list: " + e.getMessage());
+        }
+    }
 
     @Override
     public String getBotUsername() {
@@ -33,6 +53,9 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/start":
                     startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
+                case "/help":
+                    sendMessage(chatId, "This is UCS support telegram bot.");
+                    break;
                 default:
                     sendMessage(chatId, "Command is not supported.");
             }
@@ -52,7 +75,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         try {
             execute(message);
         } catch (TelegramApiException e) {
-            System.out.println(e.getMessage());
+            log.error("Something went wrong: " + e.getMessage());
         }
     }
 }
